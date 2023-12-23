84ff663bff367b158e86ba4be2c9f900de577f1d
gszp1 <grzybekmateusz@poczta.fm>
2023-12-23 22:04:39 +0100

    Changes utils class name to Utils.

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -2,7 +2,7 @@ package utils;
 
 import java.util.regex.Pattern;
 
-public class utils {
+public class Utils {
     public static final String EMAIL_PATTERN =
             "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
