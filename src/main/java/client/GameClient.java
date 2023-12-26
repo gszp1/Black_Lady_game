633f1109f669c35d234b80ded10a8edf0b307f88
gszp1 <grzybekmateusz@poczta.fm>
2023-12-26 20:46:48 +0100

    Moved login and registration to serverSide

--- src/main/java/client/GameClient.java
+++ src/main/java/client/GameClient.java
@@ -9,59 +9,7 @@ import java.util.regex.Matcher;
 
 public class GameClient {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
     }
-
-    private void registerUser(String email, String username, String password, String passwordConfirmation)
-            throws registrationFailureException{
-        Optional<String> validationResult = validateRegistrationCredentials(email, username,
-                password, passwordConfirmation);
-        if(validationResult.isPresent()) {
-            throw(new registrationFailureException(validationResult.get()));
-        }
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new registrationFailureException("Failed to connect with service.");
-        }
-
-    }
-
-    private void loginUser(String email, String password) throws loginFailureException {
-        Optional<String> validationResult = validateLoginCredentials(email, password);
-        if(validationResult.isPresent()) {
-            throw(new loginFailureException(validationResult.get()));
-        }
-    }
-
-    private Optional<String> validateLoginCredentials(String email, String password) {
-        if(email.isEmpty() || password.isEmpty()) {
-            return Optional.of("Not all login data provided.");
-        }
-        Matcher matcher = Utils.pattern.matcher(email);
-        if(!matcher.matches()) {
-            return Optional.of("Provided Email is incorrect.");
-        }
-        return Optional.empty();
-    }
-
-    private Optional<String> validateRegistrationCredentials(String email, String username,
-                                                             String password, String passwordConfirmation) {
-        if(username.isEmpty() || email.isEmpty() || password.isEmpty() || passwordConfirmation.isEmpty()) {
-            return Optional.of("Not all registration data provided.");
-        }
-        if(!passwordConfirmation.equals(password)) {
-            return Optional.of("Provided passwords are not equal.");
-        }
-        Matcher matcher = Utils.pattern.matcher(email);
-        if(!matcher.matches()) {
-            return Optional.of("Provided Email is incorrect.");
-        }
-
-        return Optional.empty();
-    }
-
 }
-
-
