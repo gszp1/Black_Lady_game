2d39b228a30369efd55ce35fdad8ad74fa76ab46
gszp1 <grzybekmateusz@poczta.fm>
2023-12-27 00:24:57 +0100

    Makes email and username fields for user entry unique

--- Users.sql
+++ Users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users(
       UserID int not null AUTO_INCREMENT PRIMARY KEY,
-      email varchar(128) NOT NULL,
-      username varchar(128) NOT NULL,
+      email varchar(128) NOT NULL UNIQUE,
+      username varchar(128) NOT NULL UNIQUE,
       password varchar(32) NOT NULL
 );
