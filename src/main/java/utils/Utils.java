9d46a1b6c284956a04ca626df3873dd359280e76
gszp1 <grzybekmateusz@poczta.fm>
2023-12-27 01:42:36 +0100

    Adds JavaDoc for Utils class | Removes redundant imports from Utils class.

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,9 +1,11 @@
 package utils;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.regex.Pattern;
 
+/**
+ * Class for universal code, not belonging specifically to any other class.
+ * Contains pattern for email validation and compiled pattern.
+ */
 public class Utils {
     public static final String EMAIL_PATTERN =
             "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
