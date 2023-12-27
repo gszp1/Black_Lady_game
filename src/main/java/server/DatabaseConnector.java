b3cfd6b842fe903f254a59f78a93218d14030428
gszp1 <grzybekmateusz@poczta.fm>
2023-12-27 01:36:11 +0100

    Adds JavaDoc for Database Connector

--- src/main/java/server/DatabaseConnector.java
+++ src/main/java/server/DatabaseConnector.java
@@ -3,6 +3,10 @@ package server;
 import java.sql.*;
 import java.util.ArrayList;
 
+/**
+ * Class which provides interface for connection between server and MySQL database.
+ */
+
 public class DatabaseConnector {
 
     private final String DB_USERNAME = "root";
@@ -15,6 +19,11 @@ public class DatabaseConnector {
 
     private final PreparedStatement insertStatement;
 
+    /**
+     * Constructor, establishes connection with MySQL database with ip: 127.0.0.1 and port 3306, called Users_database.
+     * Creates prepared statements.
+     * @throws Exception - Cumulative exception, all exceptions are handled as connection error.
+     */
     DatabaseConnector() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
         connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Users_database",
@@ -28,6 +37,12 @@ public class DatabaseConnector {
         insertStatement = connection.prepareStatement("INSERT INTO users (email, username, password) VALUES (?, ?, ?) ");
     }
 
+    /**
+     * Sends SELECT query to database in order to get user given my email argument.
+     * @param email - email of searched user.
+     * @return - ArrayList of record parameters in forms of strings.
+     * @throws SQLException - Thrown when error happens during query lifetime.
+     */
     public ArrayList<String> getUserFromDatabase(String email) throws SQLException {
         ArrayList<String> fields = new ArrayList<>();
         selectStatement.setString(1, email);
@@ -42,6 +57,14 @@ public class DatabaseConnector {
         return fields;
     }
 
+    /**
+     * Sends INSERT query to database in order to add a new user to users table.
+     * @param email - New user's email, must be unique for all records.
+     * @param username - New user's username, must be unique for all records.
+     * @param password - New user's password hashed with md5 encryption.
+     * @return - Number of inserted rows.
+     * @throws SQLException - Thrown when error happens during query lifetime.
+     */
     public int insertUserIntoDatabase(String email, String username, String password) throws SQLException {
         insertStatement.setString(1, email);
         insertStatement.setString(2, username);
