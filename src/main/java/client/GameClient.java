5c36ca367ad6f2b25ba5f125cda887bb4961bc2d
gszp1 <grzybekmateusz@poczta.fm>
2023-12-27 22:14:17 +0100

    Removes volume creation for tests database | Changes network for tests containers.

--- src/main/java/client/GameClient.java
+++ src/main/java/client/GameClient.java
@@ -1,8 +1,50 @@
 package client;
 
+//import javafx.application.Application;
+//import javafx.geometry.Insets;
+//import javafx.scene.Scene;
+//import javafx.scene.control.*;
+//import javafx.scene.layout.GridPane;
+//import javafx.stage.Stage;
+
 public class GameClient {
-
-    public static void main(String[] args) {
-
-    }
+//
+//    @Override
+//    public void start(Stage primaryStage) {
+//        primaryStage.setTitle("Login page");
+//
+//        // Create a GridPane layout
+//        GridPane grid = new GridPane();
+//        grid.setHgap(10);
+//        grid.setVgap(10);
+//        grid.setPadding(new Insets(20, 20, 20, 20));
+//
+//        // Create UI components
+//        Label emailLabel = new Label("Email:");
+//        TextField emailTextField = new TextField();
+//        Label passwordLabel = new Label("Password:");
+//        PasswordField passwordField = new PasswordField();
+//        Button loginButton = new Button("Login");
+//        Button registerButton = new Button("Register");
+//
+//        // Add components to the GridPane
+//        grid.add(emailLabel, 0, 0);
+//        grid.add(emailTextField, 1, 0);
+//        grid.add(passwordLabel, 0, 1);
+//        grid.add(passwordField, 1, 1);
+//        grid.add(loginButton, 0, 3);
+//        grid.add(registerButton, 1, 3);
+//
+//        // Create a Scene and set it on the Stage
+//        Scene scene = new Scene(grid, 400, 200);
+//        primaryStage.setScene(scene);
+//
+//        // Show the Stage
+//        primaryStage.show();
+//    }
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
 }
+
