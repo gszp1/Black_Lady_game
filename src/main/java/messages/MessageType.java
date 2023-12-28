e0136ff535f98d4cbaee97d5d68486be898e5234
gszp1 <grzybekmateusz@poczta.fm>
2023-12-28 20:19:37 +0100

    Adds JavaDoc for message type.

--- src/main/java/messages/MessageType.java
+++ src/main/java/messages/MessageType.java
@@ -1,17 +1,31 @@
 package messages;
 
+/**
+ * Enumerate for all types of messages in server-client connection.
+ */
 public enum MessageType {
     LoginRequest("LoginRequest"),
     LoginResponse("LoginResponse"),
     RegisterRequest("RegisterRequest"),
     RegisterResponse("RegisterResponse");
 
+    /**
+     * String depicting message type.
+     */
     private final String typeName;
 
+    /**
+     * Constructor for enumerate. Sets the typeName field.
+     * @param typeName - String depicting message type.
+     */
     MessageType(String typeName) {
         this.typeName = typeName;
     }
 
+    /**
+     * Getter for message type name.
+     * @return - String depicting message type.
+     */
     public String getTypeName() {
         return typeName;
     }
