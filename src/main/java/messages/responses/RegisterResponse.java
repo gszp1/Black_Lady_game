b61d9a5a78027e46a4e5b896750ee7e3f5f0d591
gszp1 <grzybekmateusz@poczta.fm>
2023-12-28 23:41:31 +0100

    Adds JavaDoc for RegisterResponse.

--- src/main/java/messages/responses/RegisterResponse.java
+++ src/main/java/messages/responses/RegisterResponse.java
@@ -7,12 +7,25 @@ import java.io.IOException;
 
 //todo: adapt to needs when implementing register
 
+/**
+ * Class for register response message.
+ */
 public class RegisterResponse extends Message {
 
+    /**
+     * Constructor for register response.
+     * @param data - Information contained by message.
+     * @param userID - ID of user to whom the message is addressed.
+     */
     public RegisterResponse(String data, int userID) {
         super(MessageType.RegisterResponse, data, userID);
     }
 
+    /**
+     * Register response handling procedure.
+     * @return - Boolean telling if operation was successful.
+     * @throws IOException - Exception thrown if something went wrong with sending the message.
+     */
     @Override
     public boolean handleMessage() throws IOException {
         return false;
