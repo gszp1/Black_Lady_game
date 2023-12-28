9ab3d92e850b22650bc079a679003c0d839d26b6
gszp1 <grzybekmateusz@poczta.fm>
2023-12-28 18:14:09 +0100

    Adds JavaDoc for Deck class.

--- src/main/java/cards/Deck.java
+++ src/main/java/cards/Deck.java
@@ -2,10 +2,21 @@ package cards;
 
 import java.util.ArrayList;
 
+/**
+ * Class representing cards deck.
+ */
+
 public class Deck {
 
+    /**
+     * Array list which contains all cards in deck.
+     */
     private final ArrayList<Card> cards;
 
+
+    /**
+     * Constructor, creates ArrayList with cards, and adds these cards to it.
+     */
     public Deck() {
         cards = new ArrayList<>();
         for (CardType cardType: CardType.values()) {
@@ -15,6 +26,10 @@ public class Deck {
         }
     }
 
+    /**
+     * Get ArrayList containing all cards in the deck.
+     * @return - Array list of cards.
+     */
     public ArrayList<Card> getCards() {
         return cards;
     }
