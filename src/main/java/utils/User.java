e14f9f151ea09690c891c45d381acb56a777f5b0
gszp1 <grzybekmateusz@poczta.fm>
2023-12-28 19:16:51 +0100

    Adds JavaDoc for Client class.

--- src/main/java/utils/User.java
+++ src/main/java/utils/User.java
@@ -4,18 +4,45 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+/**
+ * Class containing all data required for handling clients who connect with our service.
+ */
+
 public class User {
 
+    /**
+     * ID of a user - Taken from database, unique.
+     */
     private final String userID;
 
+    /**
+     * Email of a user - Taken from database, unique.
+     */
     private final String email;
 
+    /**
+     * User's username - Taken from database, unique.
+     */
     private final String username;
 
+    /**
+     * OutputStream used for sending messages to user's client app.
+     */
     private final ObjectOutputStream outputStream;
 
+    /**
+     * Reference to socket created upon establishing connection.
+     */
     private final Socket socket;
 
+    /**
+     * Constructor, fills are needed class fields, wraps socket's output stream with ObjectOutputStream.
+     * @param userID - user's ID.
+     * @param email - user's Email.
+     * @param username - user's username.
+     * @param socket - Server side socket, to which client's app socket is connected.
+     * @throws IOException - Input Output Exception thrown when it's impossible to open output stream.
+     */
     public User(String userID, String email, String username, Socket socket) throws IOException {
         this.userID = userID;
         this.email = email;
@@ -24,18 +51,34 @@ public class User {
         outputStream = new ObjectOutputStream(socket.getOutputStream());
     }
 
+    /**
+     * Getter for userID.
+     * @return - user's ID.
+     */
     public String getUserID() {
         return userID;
     }
 
+    /**
+     * Getter for user's email.
+     * @return - user's email.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Getter for server side socket handling connection with client.
+     * @return - socket to which client is connected.
+     */
     public Socket getSocket() {
         return socket;
     }
 
+    /**
+     * Getter for ObjectOutputStream wrapping socket OutputStream.
+     * @return - reference for socket's ObjectOutputStream.
+     */
     public ObjectOutputStream getOutputStream() {
         return outputStream;
     }
