1cd2fc49a6134b79c8db2158a32c4b5b1aea681c
gszp1 <grzybekmateusz@poczta.fm>
2023-12-28 18:45:40 +0100

    Adds User class.

--- src/main/java/utils/User.java
+++ src/main/java/utils/User.java
@@ -1,4 +1,42 @@
 package utils;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class User {
+
+    private final String userID;
+
+    private final String email;
+
+    private final String username;
+
+    private final ObjectOutputStream outputStream;
+
+    private final Socket socket;
+
+    public User(String userID, String email, String username, Socket socket) throws IOException {
+        this.userID = userID;
+        this.email = email;
+        this.username = username;
+        this.socket = socket;
+        outputStream = new ObjectOutputStream(socket.getOutputStream());
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public ObjectOutputStream getOutputStream() {
+        return outputStream;
+    }
 }
