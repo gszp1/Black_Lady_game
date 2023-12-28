e105ba03785a17149025ac65521604774f427cdc
gszp1 <grzybekmateusz@poczta.fm>
2023-12-28 20:42:08 +0100

    Adds JavaDoc for Message class.

--- src/main/java/messages/Message.java
+++ src/main/java/messages/Message.java
@@ -3,30 +3,66 @@ package messages;
 import java.io.IOException;
 import java.io.Serializable;
 
+
+/**
+ * Abstract class representing messages sent between server and client.
+ */
 public abstract class Message implements Serializable {
 
+    /**
+     *  Type of message.
+     */
     private final MessageType messageType;
 
+    /**
+     * Information passed with message.
+     */
     private final String data;
 
+    /**
+     * ID of client who sent the message / to whom the message is being sent.
+     */
     private final int clientID;
 
+    /**
+     * Constructor for message.
+     * @param messageType - Type of message.
+     * @param data - Information passed with message.
+     * @param clientID - ID of client.
+     */
     public Message(MessageType messageType, String data, int clientID) {
         this.messageType = messageType;
         this.clientID = clientID;
         this.data = data;
     }
 
+    /**
+     * Message handling procedure, behaviour defined by type of message.
+     * @return - Returns boolean describing result of message handling.
+     * @throws IOException - Exception thrown if something went wrong with sending message.
+     */
     public abstract boolean handleMessage() throws IOException;
 
+    /**
+     * Getter for message type.
+     * @return - Message type.
+     */
     public MessageType getMessageType() {
         return messageType;
     }
 
+    /**
+     * Getter for message's information.
+     * @return - Message information.
+     */
     public String getData() {
         return data;
     }
 
+    /**
+     * Getter for client's ID.
+     * @return - ID of client.
+     */
     public int getClientID() {
         return clientID;
     }
