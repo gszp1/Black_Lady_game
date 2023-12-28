e5192543bf79630f7e272d494c2c2e269aeeac9c
gszp1 <grzybekmateusz@poczta.fm>
2023-12-28 23:22:09 +0100

    Adds JavaDoc to LoginResponse.

--- src/main/java/messages/responses/LoginResponse.java
+++ src/main/java/messages/responses/LoginResponse.java
@@ -7,11 +7,25 @@ import java.io.IOException;
 
 //todo: adapt to needs when implementing login
 
+/**
+ * Class for login response message.
+ */
 public class LoginResponse extends Message {
 
+    /**
+     * Constructor for login response.
+     * @param data - Information contained by message.
+     * @param userID - ID of user to whom the message is addressed.
+     */
     public LoginResponse(String data, int userID) {
         super(MessageType.LoginResponse, data, userID);
     }
+
+    /**
+     * Login response handling procedure.
+     * @return - Boolean telling if operation was successful.
+     * @throws IOException - Exception thrown if something went wrong with sending the message.
+     */
     @Override
     public boolean handleMessage() throws IOException {
         return false;
