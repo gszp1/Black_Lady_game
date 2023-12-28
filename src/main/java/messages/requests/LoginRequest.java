26077b58989bf2ab83ce094ec2c390230dd526cd
gszp1 <grzybekmateusz@poczta.fm>
2023-12-28 22:02:36 +0100

    Adds JavaDoc for LoginRequest.

--- src/main/java/messages/requests/LoginRequest.java
+++ src/main/java/messages/requests/LoginRequest.java
@@ -5,17 +5,35 @@ import messages.MessageType;
 
 import java.io.IOException;
 
+/**
+ * Class for login request message.
+ */
 public class LoginRequest extends Message {
-
-    public LoginRequest(MessageType messageType, String email, String password, int clientID) {
-        super(messageType, String.format("%s|%s", email, password), clientID);
+    /**
+     * Constructor for login request message.
+     * @param email - User's email
+     * @param username - User's username.
+     * @param password - User's password.
+     * @param clientID - Client's ID.
+     */
+    public LoginRequest(String email, String username, String password, int clientID) {
+        super(MessageType.LoginRequest, String.format("%s|%s|%S", email, username, password), clientID);
     }
 
+    /**
+     * Login Request handling procedure, behaviour defined by type of message.
+     * @return - Returns boolean describing result of message handling.
+     * @throws IOException - Exception thrown if something went wrong with sending message.
+     */
     @Override
     public boolean handleMessage() throws IOException {
         return false;
     }
 
+    /**
+     * Parses data field to String array with email and password.
+     * @return - String array with email and password.
+     */
     public String [] parseData() {
         return getData().split("\\|");
     }
