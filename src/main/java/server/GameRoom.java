1fd8257621fb45f9297f74ebe1970d7ebd209cf8
gszp1 <grzybekmateusz@poczta.fm>
2023-12-28 19:21:41 +0100

    Adds Deck to GameRoom.

--- src/main/java/server/GameRoom.java
+++ src/main/java/server/GameRoom.java
@@ -1,5 +1,6 @@
 package server;
 
+import cards.Deck;
 import utils.User;
 
 import java.util.ArrayList;
@@ -8,10 +9,13 @@ public class GameRoom {
 
     private final int MAX_USERS = 4;
 
+    private final Deck cardDeck;
+
     private final ArrayList<User> users;
 
     public GameRoom() {
         users = new ArrayList<>();
+        cardDeck = new Deck();
     }
 
     public boolean addUser(User user) {
