168eed46add1aefb5548edd3e0f250e083559a37
gszp1 <grzybekmateusz@poczta.fm>
2023-12-31 01:50:30 +0100

    Adds JavaDoc for ServerSocketConnectionExceptio | Adds InputHandler class.

--- src/main/java/exceptions/ServerSocketConnectionException.java
+++ src/main/java/exceptions/ServerSocketConnectionException.java
@@ -1,17 +1,32 @@
 package exceptions;
 
+
+/**
+ * Exception for server side errors occurring during server-client communication.
+ */
 public class ServerSocketConnectionException {
 
     public static final String UNKNOWN_MESSAGE_TYPE = "Message received from client is of unknown type.";
 
     public static final String MESSAGE_READING_FAILURE = "Failed to read message from client.";
 
+    /**
+     * String depicting cause of exception.
+     */
     private final String exceptionCause;
 
+    /**
+     * Constructor, sets cause of exception.
+     * @param exceptionCause - Cause of exception.
+     */
     public ServerSocketConnectionException(String exceptionCause) {
         this.exceptionCause = exceptionCause;
     }
 
+    /**
+     * Getter for exceptionCause;
+     * @return - String depicting cause of exception.
+     */
     public String getExceptionCause() {
         return exceptionCause;
     }
--- src/main/java/server/InputHandler.java
+++ src/main/java/server/InputHandler.java
@@ -1,4 +1,44 @@
 package server;
 
-public class InputHandler {
+import messages.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class InputHandler extends Thread{
+
+    private final String userID;
+
+    private final ObjectInputStream inputStream;
+
+    private final ConcurrentLinkedQueue<Message> inputQueue;
+
+    public InputHandler(String userID, ObjectInputStream inputStream, ConcurrentLinkedQueue<Message> inputQueue) {
+        this.userID = userID;
+        this.inputStream = inputStream;
+        this.inputQueue = inputQueue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!interrupted()) {
+                Message message = (Message) inputStream.readObject();
+                message.setClientID(userID);
+                inputQueue.add(message);
+            }
+        } catch (ClassNotFoundException e) {
+
+        } catch (IOException e) {
+
+        }
+    }
+
+
+
+
+
+
+    
 }
