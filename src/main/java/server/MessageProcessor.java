9ff664e08e835b6bb2fd91066e86627a99215308
gszp1 <grzybekmateusz@poczta.fm>
2024-01-02 00:40:45 +0100

    Adds JavaDoc to MessageProcessor.

--- src/main/java/server/MessageProcessor.java
+++ src/main/java/server/MessageProcessor.java
@@ -7,17 +7,34 @@ import utils.UserList;
 import java.io.IOException;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+/**
+ * Class for handling messages.
+ */
 public class MessageProcessor extends Thread{
 
+    /**
+     * Queue with messages to be handled.
+     */
     private final ConcurrentLinkedQueue<Message> inputQueue;
 
+    /**
+     * UserList containing all users.
+     */
     private final UserList userList;
 
+    /**
+     * Constructor, sets inputQueue and userList with given reference.
+     * @param inputQueue - Reference to inputQueue.
+     * @param userList - Reference to userList.
+     */
     public MessageProcessor(ConcurrentLinkedQueue<Message> inputQueue, UserList userList) {
         this.inputQueue = inputQueue;
         this.userList = userList;
     }
 
+    /**
+     * Implementation of Thread.run() abstract method.
+     */
     @Override
     public void run() {
         try {
