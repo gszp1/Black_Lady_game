540eb249524884f3fb3067b41b6d726ffe3fdcd5
gszp1 <grzybekmateusz@poczta.fm>
2024-01-09 02:55:22 +0100

    Adds user removal in LogoutRequest.

--- src/main/java/messages/requests/LogoutRequest.java
+++ src/main/java/messages/requests/LogoutRequest.java
@@ -6,6 +6,7 @@ import server.DatabaseConnector;
 import utils.User;
 import utils.UserList;
 
+import javax.swing.text.html.Option;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Optional;
@@ -25,6 +26,14 @@ public class LogoutRequest extends Message {
 
     @Override
     public boolean handleMessage(UserList userList, DatabaseConnector databaseConnector) throws IOException, SQLException {
+        Optional<User> userWrapper = userList.getUser(this.getClientID());
+        if (userWrapper.isPresent()) {
+            User user = userWrapper.get();
+            //todo remove user from all game rooms.
+            userList.removeUser(user);
+        }
+
         return true;
     }
+
 }
