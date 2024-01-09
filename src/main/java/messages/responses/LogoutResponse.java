10afcf2ef9faf274149b134aea63b4997c361075
gszp1 <grzybekmateusz@poczta.fm>
2024-01-09 13:57:29 +0100

    Adds JavaDoc for LogoutResponse.

--- src/main/java/messages/responses/LogoutResponse.java
+++ src/main/java/messages/responses/LogoutResponse.java
@@ -8,20 +8,30 @@ import utils.UserList;
 import java.io.IOException;
 import java.sql.SQLException;
 
+/**
+ * Class for logout response.
+ */
 public class LogoutResponse extends Message {
     /**
      * Constructor for LogoutResponse.
-     *
-     * @param messageType - Type of message.
-     * @param data        - Information passed with message.
-     * @param clientID    - ID of client.
+     * @param messageType Type of message.
+     * @param data Information passed with message.
+     * @param clientID ID of client.
      */
     public LogoutResponse(MessageType messageType, String data, String clientID) {
         super(messageType, data, clientID);
     }
 
+    /**
+     * Logout response message handling procedure.
+     * @param userList List of users.
+     * @param databaseConnector Connection to database.
+     * @return result of message handling: true or false.
+     * @throws IOException Thrown if something went wrong with server-user connection.
+     * @throws SQLException Thrown if something went wrong with database connection.
+     */
     @Override
     public boolean handleMessage(UserList userList, DatabaseConnector databaseConnector) throws IOException, SQLException {
-        return false;
+        return true;
     }
 }
