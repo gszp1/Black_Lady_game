2015575a2a0e413232ea4ac8bdb6bca805682788
gszp1 <grzybekmateusz@poczta.fm>
2024-01-09 13:52:50 +0100

    Adds JavaDoc to RegisterRequest.

--- src/main/java/messages/requests/RegisterRequest.java
+++ src/main/java/messages/requests/RegisterRequest.java
@@ -28,6 +28,14 @@ public class RegisterRequest extends Message {
 
     public static String REGISTRATION_SUCCESS_RESPONSE = "Registration successful.";
 
+    /**
+     * Constructor for RegisterRequest.
+     * @param email New user email.
+     * @param username New user username.
+     * @param password New user password.
+     * @param passwordConfirmation Password confirmation, must be equal to password.
+     * @param clientID ID of user who sent the message.
+     */
     public RegisterRequest(String email, String username, String password,
                            String passwordConfirmation, String clientID) {
         super(MessageType.RegisterRequest, String.format("%s|%s|%s|%s", email, username, password, passwordConfirmation), clientID);
@@ -58,11 +66,21 @@ public class RegisterRequest extends Message {
         return true;
     }
 
+    /**
+     * Validates given email.
+     * @param email Email to be validated.
+     * @return true if email is correct, otherwise false.
+     */
     private boolean validateEmail(String email) {
         Matcher matcher = Utils.pattern.matcher(email);
         return matcher.matches();
     }
 
+    /**
+     * Validates credentials stored in message content.
+     * @param credentials Credentials after parsing.
+     * @throws RegistrationFailureException Thrown if credentials are invalid.
+     */
     private void validateCredentials(String [] credentials) throws RegistrationFailureException {
         if (credentials[0].isEmpty() || credentials[1].isEmpty() || credentials[2].isEmpty() || credentials[3].isEmpty()) {
             throw new RegistrationFailureException(RegistrationFailureException.EMPTY_FIELDS);
@@ -75,6 +93,13 @@ public class RegisterRequest extends Message {
         }
     }
 
+    /**
+     * Sends response to user who sent this message.
+     * @param result String stating result of operation: Success or Failure.
+     * @param responseContent Message to be sent together with result.
+     * @param userList List of users.
+     * @throws IOException Exception thrown if failed to send the message.
+     */
     private void sendResponse(String result, String responseContent, UserList userList) throws IOException {
         Optional<User> user = userList.getUser(this.getClientID());
         if (user.isPresent()) {
