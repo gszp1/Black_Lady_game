800ce7c3567927bce83af78619c0fc94983d8001
gszp1 <grzybekmateusz@poczta.fm>
2024-01-09 02:11:05 +0100

    Adds constructor and handleMessage to LogoutRequest.

--- src/main/java/messages/requests/LogoutRequest.java
+++ src/main/java/messages/requests/LogoutRequest.java
@@ -1,4 +1,30 @@
 package messages.requests;
 
-public class LogoutRequest {
+import messages.Message;
+import messages.MessageType;
+import server.DatabaseConnector;
+import utils.User;
+import utils.UserList;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class LogoutRequest extends Message {
+
+    /**
+     * Constructor LogoutRequest.
+     *
+     * @param messageType - Type of message.
+     * @param data        - Information passed with message.
+     * @param clientID    - ID of client.
+     */
+    public LogoutRequest(MessageType messageType, String data, String clientID) {
+        super(messageType, data, clientID);
+    }
+
+    @Override
+    public boolean handleMessage(UserList userList, DatabaseConnector databaseConnector) throws IOException, SQLException {
+        return false;
+    }
 }
