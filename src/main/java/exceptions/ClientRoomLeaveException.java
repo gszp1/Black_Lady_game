2aa34eac2341ab01c35f49d4e8d99acecbc0fe36
gszp1 <grzybekmateusz@poczta.fm>
2024-01-21 22:22:07 +0100

    Adds JD for ClientRoomLeaveException.

--- src/main/java/exceptions/ClientRoomLeaveException.java
+++ src/main/java/exceptions/ClientRoomLeaveException.java
@@ -1,17 +1,37 @@
 package exceptions;
 
+/**
+ * Exception for errors during leaving game rooms.
+ */
 public class ClientRoomLeaveException extends Exception {
 
+    /**
+     * Error message if user can't leave the room.
+     */
     public static final String LEAVE_FAIL = "Failed to leave room.";
-
+    
+    /**
+     * Error message if user is not in the room.
+     */
     public static final String USER_NOT_IN_ROOM = "User is not in this room.";
 
+    /**
+     * Cause of error.
+     */
     private final String errorCause;
 
+    /**
+     * Constructor, sets error cause.
+     * @param errorCause
+     */
     public ClientRoomLeaveException(String errorCause) {
         this.errorCause = errorCause;
     }
 
+    /**
+     * Getter for error cause.
+     * @return Error cause.
+     */
     public String getErrorCause() {
         return errorCause;
     }
