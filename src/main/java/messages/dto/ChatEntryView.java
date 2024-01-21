36b15c33d94a59255283ba71a4cdb582c95366c4
gszp1 <grzybekmateusz@poczta.fm>
2024-01-21 23:03:48 +0100

    Adds JD for ChatEntryView.

--- src/main/java/messages/dto/ChatEntryView.java
+++ src/main/java/messages/dto/ChatEntryView.java
@@ -6,17 +6,30 @@ import utils.model.ChatEntry;
 
 import java.io.Serializable;
 
+/**
+ * Data transfer object for chat entry.
+ */
 @Data
 @Builder
 public class ChatEntryView implements Serializable {
     private final String email;
     private final String message;
 
+    /**
+     * Constructor sets email and message.
+     * @param email Entry email.
+     * @param message Entry message.
+     */
     public ChatEntryView(String email, String message) {
         this.email = email;
         this.message = message;
     }
 
+    /**
+     * Creates data transfer object using chat entry.
+     * @param chatEntry Chat entry.
+     * @return Chat entry data transfer object.
+     */
     public static ChatEntryView fromChatEntry(ChatEntry chatEntry) {
         return ChatEntryView.builder()
                 .email(chatEntry.getEmail())
