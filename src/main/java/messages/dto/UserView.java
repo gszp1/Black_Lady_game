bc76c84f8fb46bdea1e2d52d09929eb6274b84f8
gszp1 <grzybekmateusz@poczta.fm>
2024-01-21 23:50:49 +0100

    Adds JD for UserView.

--- src/main/java/messages/dto/UserView.java
+++ src/main/java/messages/dto/UserView.java
@@ -7,13 +7,29 @@ import utils.User;
 
 import java.io.Serializable;
 
+/**
+ *  Data transfer object for representing user information.
+ */
 @Data
 @EqualsAndHashCode
 @Builder
 public class UserView implements Serializable {
+    /**
+     * User's email.
+     */
     private String email;
+
+    /**
+     * Boolean stating if data belong to the user who receives data.
+     */
     private boolean isMe;
 
+    /**
+     * Creates a UserView instance from a User object.
+     * @param user User data.
+     * @param loggedInUserId User's ID.
+     * @return UserView instance.
+     */
     public static UserView fromUser(User user, String loggedInUserId) {
         return UserView.builder()
                 .email(user.getEmail())
