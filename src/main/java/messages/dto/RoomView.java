b612d6a31e931047714e11fe5a403a225b0499a7
gszp1 <grzybekmateusz@poczta.fm>
2024-01-22 00:24:19 +0100

    Adds JD for RoomView.

--- src/main/java/messages/dto/RoomView.java
+++ src/main/java/messages/dto/RoomView.java
@@ -9,15 +9,44 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Data transfer object for information about a room.
+ */
 @Data
 @Builder
 public class RoomView implements Serializable {
+
+    /**
+     * ID of room.
+     */
     private final int roomId;
+
+    /**
+     * Number of players in room.
+     */
     private final int numberOfParticipants;
+
+    /**
+     * Email of user who created room.
+     */
     private final String roomCreatorEmail;
+
+    /**
+     * Flag stating if user is in, or not in room.
+     */
     private final boolean isUserInRoom;
+
+    /**
+     * Flag stating is user created this room.
+     */
     private final boolean isUserCreator;
 
+    /**
+     * Creates RoomView with data stored in Room instance.
+     * @param room Room with data used to create RoomView.
+     * @param loggedInUserId ID of logged-in user.
+     * @return RoomView instance.
+     */
     public static RoomView fromRoom(Room room, String loggedInUserId) {
         return RoomView.builder()
                 .isUserCreator(isUserCreator(room, loggedInUserId))
@@ -28,10 +57,22 @@ public class RoomView implements Serializable {
                 .build();
     }
 
+    /**
+     * Checks if given user is room's creator.
+     * @param room Room.
+     * @param loggedInUserId ID of checked user.
+     * @return Boolean stating if user is a creator of room.
+     */
     private static boolean isUserCreator(Room room, String loggedInUserId) {
         return room.getOwner().getUserID().equals(loggedInUserId);
     }
 
+    /**
+     * Checks if user is in room.
+     * @param room Room in which user is searched for.
+     * @param loggedInUserId ID of searched user.
+     * @return Boolean stating if user is in room.
+     */
     private static boolean isUserInRoom(Room room, String loggedInUserId) {
         final List<String> participantIds = room.getParticipants().stream()
                 .map(User::getUserID)
