8541356999348b94888f0214e27f30cc7cfc8f36
gszp1 <grzybekmateusz@poczta.fm>
2024-01-21 22:41:49 +0100

    Adds JD for RequestFailureException.

--- src/main/java/exceptions/RequestFailureException.java
+++ src/main/java/exceptions/RequestFailureException.java
@@ -1,11 +1,24 @@
 package exceptions;
 
+/**
+ * Exception for request failure.
+ */
 public class RequestFailureException extends Exception {
 
+    /**
+     * Error for message receiver being not found.
+     */
     public static final String MESSAGE_RECEIVER_NOT_FOUND = "Failed to find user with such credentials.";
 
+    /**
+     * Cause of error.
+     */
     private final String errorCause;
 
+    /**
+     * Constructor, sets error cause.
+     * @param errorCause
+     */
     public RequestFailureException(String errorCause) {
         this.errorCause = errorCause;
     }
