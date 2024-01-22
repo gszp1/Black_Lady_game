0fabab60fc6bc529c23c6b16502d51e4d7e4effa
gszp1 <grzybekmateusz@poczta.fm>
2024-01-22 02:14:07 +0100

    GameDetailsResponse.

--- src/main/java/messages/toClient/responses/GameDetailsResponse.java
+++ src/main/java/messages/toClient/responses/GameDetailsResponse.java
@@ -6,18 +6,31 @@ import messages.dto.GameView;
 import messages.toClient.ToClientMessage;
 
 import java.io.IOException;
-import java.util.Objects;
 
+/**
+ * Response for game details request.
+ */
 @Getter
 public class GameDetailsResponse extends ToClientMessage {
-
+    /**
+     *  Game room data.
+     */
     private final GameView gameView;
 
+    /**
+     * Constructor, sets data and game room data.
+     * @param gameView
+     */
     public GameDetailsResponse(GameView gameView) {
         super(MessageType.GameDetailsResponse, gameView.toString());
         this.gameView = gameView;
     }
 
+    /**
+     * Default handling procedure.
+     * @return false;
+     * @throws IOException IOException.
+     */
     @Override
     public boolean handle() throws IOException {
         return false;
