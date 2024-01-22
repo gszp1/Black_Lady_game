d94fb71d07576a078d914a229315b385960eced1
gszp1 <grzybekmateusz@poczta.fm>
2024-01-22 02:56:16 +0100

    Adds JD for RoomInviteToReceiverRequest.

--- src/main/java/messages/toClient/responses/RoomInviteToReceiverRequest.java
+++ src/main/java/messages/toClient/responses/RoomInviteToReceiverRequest.java
@@ -6,15 +6,33 @@ import messages.toClient.ToClientMessage;
 
 import java.io.IOException;
 
+/**
+ * Invitation request to client.
+ */
 @Getter
 public class RoomInviteToReceiverRequest extends ToClientMessage {
 
+    /**
+     * Email of invitation receiver.
+     */
     private final String receiverEmail;
 
+    /**
+     * Email of invitation sender.
+     */
     private final String senderEmail;
 
+    /**
+     * ID of room.
+     */
     private final int roomId;
 
+    /**
+     * Constructor, sets receiver email, sender email and room id.
+     * @param receiverEmail Email of invitation receiver.
+     * @param senderEmail Email of invitation sender.
+     * @param roomId ID of room.
+     */
     public RoomInviteToReceiverRequest(String receiverEmail, String senderEmail, int roomId) {
         super(MessageType.RoomInviteToReceiverRequest, String.format("%s|%s", senderEmail, roomId));
         this.receiverEmail = receiverEmail;
@@ -22,6 +40,11 @@ public class RoomInviteToReceiverRequest extends ToClientMessage {
         this.roomId = roomId;
     }
 
+    /**
+     * Default handling procedure.
+     * @return false.
+     * @throws IOException IOException.
+     */
     @Override
     public boolean handle() throws IOException {
         return false;
