c8bcb7178b213dc7e68f0790bcb9b81701c0a856
gszp1 <grzybekmateusz@poczta.fm>
2024-01-22 09:30:18 +0100

    Adds JD for Utils.

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -5,8 +5,6 @@ import cards.CardSet;
 import cards.CardType;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.regex.Pattern;
 import java.util.List;
 
@@ -15,15 +13,31 @@ import java.util.List;
  * Contains pattern for email validation and compiled pattern.
  */
 public class Utils {
+
+    /**
+     * Regular expression for validating emails.
+     */
     public static final String EMAIL_PATTERN =
             "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
+    /**
+     * Pattern for validating emails.
+     */
     public static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
 
+    /**
+     * Method for validating email.
+     * @param email Checked email.
+     * @return Boolean.
+     */
     public static boolean isEmailValid(String email) {
         return Pattern.matches(EMAIL_PATTERN, email);
     }
 
+    /**
+     * Generates full deck of 52 cards.
+     * @return Deck of cards.
+     */
     public static List<Card> getFullDeck() {
         List<Card> cards = new ArrayList<>();
         for (CardSet cardSet : CardSet.values()) {
