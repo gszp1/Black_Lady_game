beaea9a4a541de253c1a0cebf860b698698d3937
gszp1 <grzybekmateusz@poczta.fm>
2024-01-22 12:10:39 +0100

    Adds JD for Trick.

--- src/main/java/utils/score/Trick.java
+++ src/main/java/utils/score/Trick.java
@@ -3,10 +3,28 @@ package utils.score;
 import cards.Card;
 import lombok.Data;
 
+/**
+ * Class for trick of cards.
+ */
 @Data
 public class Trick {
+    /**
+     * First picked card.
+     */
     private Card first;
+
+    /**
+     * Second picked card.
+     */
     private Card second;
+
+    /**
+     * Third picked card.
+     */
     private Card third;
+
+    /**
+     * Fourth picked card.
+     */
     private Card fourth;
 }
