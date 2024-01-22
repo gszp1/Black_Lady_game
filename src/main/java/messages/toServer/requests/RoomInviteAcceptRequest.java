e00ce3ec6fba598669ff307c53cea89e5937dfaf
gszp1 <grzybekmateusz@poczta.fm>
2024-01-22 17:15:32 +0100

    Adds JD for RoomInviteAcceptRequest.

--- src/main/java/messages/toServer/requests/RoomInviteAcceptRequest.java
+++ src/main/java/messages/toServer/requests/RoomInviteAcceptRequest.java
@@ -3,7 +3,6 @@ package messages.toServer.requests;
 import exceptions.ClientRoomJoinException;
 import messages.MessageType;
 import messages.toClient.responses.InviteUserResponse;
-import messages.toClient.responses.RoomInviteToReceiverRequest;
 import messages.toServer.ToServerMessage;
 import server.DatabaseConnector;
 import utils.GameDetails;
@@ -15,12 +14,32 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Optional;
 
+/**
+ * Request for accepting invitation.
+ */
 public class RoomInviteAcceptRequest extends ToServerMessage {
 
+    /**
+     * Room's ID.
+     */
     private final int roomId;
+    /**
+     * Acceptor's email.
+     */
     private final String accepterEmail;
+
+    /**
+     * Inviter's email.
+     */
     private final String inviterEmail;
 
+
+    /**
+     * Constructor for message.
+     * @param roomId Room's ID.
+     * @param accepterEmail Acceptor's email.
+     * @param inviterEmail Inviter's email.
+     */
     public RoomInviteAcceptRequest(int roomId, String accepterEmail, String inviterEmail) {
         super(MessageType.RoomInviteAcceptRequest, String.format("Accept request to room %s sent from %s to %s", roomId, inviterEmail, accepterEmail), null);
         this.roomId = roomId;
@@ -28,6 +47,15 @@ public class RoomInviteAcceptRequest extends ToServerMessage {
         this.inviterEmail = inviterEmail;
     }
 
+    /**
+     * Request Handling procedure.
+     * @param userList List of users.
+     * @param databaseConnector Connection to database.
+     * @param gameDetails Data about active game rooms.
+     * @return Boolean.
+     * @throws IOException Exception for connection errors.
+     * @throws SQLException Exception for database connection error.
+     */
     @Override
     public boolean handle(UserList userList, DatabaseConnector databaseConnector, GameDetails gameDetails) throws IOException, SQLException {
         Optional<User> accepter = userList.getUserByConnectionId(getConnectionId());
@@ -57,6 +85,12 @@ public class RoomInviteAcceptRequest extends ToServerMessage {
         return true;
     }
 
+    /**
+     * Join room.
+     * @param room Room.
+     * @param accepter Invitation accepter.
+     * @return Boolean.
+     */
     private boolean joinRoom(Room room, User accepter) {
         try {
             room.join(accepter);
@@ -67,6 +101,11 @@ public class RoomInviteAcceptRequest extends ToServerMessage {
         }
     }
 
+    /**
+     * Sends error message.
+     * @param user User.
+     * @param msg Success message.
+     */
     private void sendError(User user, String msg) {
         try{
             user.getOutputStream().writeObject(new InviteUserResponse(ToServerMessage.FAILURE, msg));
@@ -75,6 +114,11 @@ public class RoomInviteAcceptRequest extends ToServerMessage {
         }
     }
 
+    /**
+     * Sends success message.
+     * @param user User.
+     * @param msg Success message.
+     */
     private void sendSuccess(User user, String msg) {
         try {
             user.getOutputStream().writeObject(new InviteUserResponse(ToServerMessage.SUCCESS, msg));
