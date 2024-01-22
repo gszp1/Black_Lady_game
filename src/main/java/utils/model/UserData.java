74437961d37c1df010f774b0a374a4dc5e11c632
gszp1 <grzybekmateusz@poczta.fm>
2024-01-22 09:55:05 +0100

    Adds JD for UserData.

--- src/main/java/utils/model/UserData.java
+++ src/main/java/utils/model/UserData.java
@@ -2,11 +2,33 @@ package utils.model;
 
 public class UserData {
 
+    /**
+     * User's ID.
+     */
     private String userId;
+
+    /**
+     * User's email.
+     */
     private String email;
+
+    /**
+     * User's username.
+     */
     private String username;
+
+    /**
+     * User's password.
+     */
     private String password;
 
+    /**
+     * Constructor, sets user data.
+     * @param userId User's ID.
+     * @param email User's email.
+     * @param username User's username.
+     * @param password User's password.
+     */
     public UserData(String userId, String email, String username, String password) {
         this.userId = userId;
         this.email = email;
@@ -14,34 +36,66 @@ public class UserData {
         this.password = password;
     }
 
+    /**
+     * Getter for userID.
+     * @return User's ID.
+     */
     public String getUserId() {
         return userId;
     }
 
+    /**
+     * Setter for userID.
+     * @param userId User's new ID.
+     */
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
+    /**
+     * Getter for user's email.
+     * @return User's email.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Setter for user's email.
+     * @param email User's new email.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Getter for user's username.
+     * @return User's username.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Setter for user's username.
+     * @return User's new username.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Getter for user's password.
+     * @return User's password.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Setter for user's password.
+     * @return User's new password.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
